b89eac070cc54c5f7ef88874af98784fe62bdbcb
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-03-14 00:16:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,7 +25,7 @@ def swap_elements(array)
   array
 end
 
-swap_elements(["blake", "ashley", "scott"]) #["blake", "scott", "ashley"]
+swap_elements(["blake", "ashley", "scott"]) 
 
 def reverse_array(array)
   array.reverse
